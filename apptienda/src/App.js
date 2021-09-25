7f05e40816afae6597419930110b5a27dcb064b5
GonzaloPezzuti <gonzalopezzuti@gmail.com>
2021-09-25 17:48:51 -0300

    .

--- apptienda/src/App.js
+++ apptienda/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Navbar from './componentes/Navbar/Navbar';
+import {BrouserRouter as Router, Swithc, Route} from 'react-router-dom'
 function App() {
 
   return (
--- apptienda/src/componentes/Navbar/Navbar.jsx
+++ apptienda/src/componentes/Navbar/Navbar.jsx
@@ -2,15 +2,32 @@ import React from 'react'
 
 const navbar = () => {
     return (
-        <nav>
-            <ul>
-                <li>home</li>
-                <li>home</li>
-                <li>home</li>
-                
-
-            </ul>
-        </nav>
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Logo</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Ofertas</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Categorias</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled">Envios</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+        </div>
     )
 }
 
