14ae372f64193a677ef056216b9ada55177def51
GonzaloPezzuti <gonzalopezzuti@gmail.com>
2021-09-25 17:07:21 -0300

    .

---
+++ apptienda/src/componentes/navbar/navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const navbar = () => {
+    return (
+        <nav>
+            <ul>
+                <li>home</li>
+                <li>home</li>
+                <li>home</li>
+                
+
+            </ul>
+        </nav>
+    )
+}
+
+export default navbar
