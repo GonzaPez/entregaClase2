af488535c1b22eaea08351265e59fb74c75cf830
GonzaloPezzuti <gonzalopezzuti@gmail.com>
2021-10-21 21:34:21 -0300

    revision

--- apptienda/src/componentes/ItemDetail/ItemDetail.jsx
+++ apptienda/src/componentes/ItemDetail/ItemDetail.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
+// import { Link } from 'react-router-dom'; 
 
-const ItemDetail = ({ autos }) => {
+const ItemDetail = ({ prod }) => {
     // console.log(autos)
     return (
         <div>
-            <div className="card" style={{width:"18rem"}}>
+            <div className="card" style={{ width: "18rem" }}>
                 <div className="card-body">
-                    <h5 className="card-title">{autos.nombre}</h5>
-                    <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" className="card-link">Card link</a>
-                    <a href="#" className="card-link">Another link</a>
+                    {/* <img src={prod.img} className="col-sm-8" alt="...imagen de producto" /> */}
+                    {/* <h2 className="card-title">{prod.nombre}</h2> */}
+                    {/* <h2 className="card-title">{prod.detalle}</h2> */}
+                    <h3 className="card-subtitle mb-2 text-muted">Oportunidad!!!</h3>
+                    <p className="card-text">Todos los modelos cuentan con amplia financiacion.</p>
+
                 </div>
             </div>
 
